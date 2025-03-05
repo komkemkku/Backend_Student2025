a5233e57f5f0a1e265aed6d6c74500079d2324f2
komkem <komkem.k@kkumail.com>
2025-03-05 13:48:30 +0700

    .

---
+++ database/migrations/Models.go
@@ -0,0 +1,20 @@
+package migrations
+
+
+func Models() []any {
+	return []any{
+
+		// (*model.Users)(nil),
+
+	}
+}
+
+func RawBeforeQueryMigrate() []string {
+	return []string{
+		`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`,
+	}
+}
+
+func RawAfterQueryMigrate() []string {
+	return []string{}
+}
